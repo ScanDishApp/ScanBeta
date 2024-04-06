67c46192efaf9723c477e0cd3e21920bf962e0ed
Xuggi <godannyyy@gmail.com>
2024-04-06 13:56:19 +0200

    UX/UI: HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import { IoCameraOutline } from 'react-icons/io5';
 import './ScreenStyle/Home.css'; // Import the external CSS file
 
 export default function Home() {
     return (
         <div className="home-container">
             <h1>HOME</h1>
-
 
             <div className="rectangle-grid">
                 <div className="rectangle">
@@ -15,10 +15,22 @@ export default function Home() {
 
             <div className="square-grid">
                 {/* Four squares */}
-                <div className="square"></div>
-                <div className="square"></div>
-                <div className="square"></div>
-                <div className="square"></div>
+                <div className="square">
+                    <div className="mini-square">😀</div> {/* Mini square added with 😀 emoji */}
+                </div>
+                <div className="square">
+                    <div className="mini-square">😀</div> {/* Mini square added with 😀 emoji */}
+                </div>
+                <div className="square">
+                    <div className="mini-square">😀</div> {/* Mini square added with 😀 emoji */}
+                </div>
+                <div className="square">
+                    <div className="mini-square">😀</div> {/* Mini square added with 😀 emoji */}
+                </div>
+            </div>
+
+            <div className="big-rectangle">
+                <IoCameraOutline size={48} />
             </div>
         </div>
     );
