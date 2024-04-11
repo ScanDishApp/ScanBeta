d0ba4ba508390632fc966f7266c3a242763f61e0
Xuggi <godannyyy@gmail.com>
2024-04-11 10:35:36 +0200

    render

--- server.mjs
+++ server.mjs
@@ -1,29 +1,26 @@
-import 'dotenv/config' ;
-import express from 'express'; 
-import USER_API from './Backend/routes/userRoutes.mjs'; 
-import BOOK_API from './Backend/routes/bookRoutes.mjs'
+import 'dotenv/config';
+import express from 'express';
+import path from 'path'; // Import path module
+import USER_API from './Backend/routes/userRoutes.mjs';
+import BOOK_API from './Backend/routes/bookRoutes.mjs';
 import errorHandler from './Backend/modules/errorHandler.mjs';
-import cors from 'cors'
+import cors from 'cors';
 
 const server = express();
 server.use(cors());
 
-const port = (process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
 server.set('port', port);
 
+// Serve static files from the 'src/screens' directory
+server.use(express.static(path.join(__dirname, 'src', 'screens')));
 
-server.use(express.static('public'));
-
-server.use("/user", USER_API); 
-
-server.use("/book", BOOK_API );
-
-
+server.use("/user", USER_API);
+server.use("/book", BOOK_API);
 
 server.get("/", (req, res, next) => {
    req.originalUrl
    res.status(200).send(JSON.stringify({ msg: "Hello there" })).end();
-
 });
 
 server.use(errorHandler);
@@ -31,5 +28,3 @@ server.use(errorHandler);
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
 });
-
-
