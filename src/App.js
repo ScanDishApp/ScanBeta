2bc317b4f764349bae8dd3077e423e4cb38a7d52
Xuggi <godannyyy@gmail.com>
2024-04-12 12:07:51 +0200

    profile to new page after login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './screens/HomeScreen';
 import Scan from './screens/ScanScreen';
 import NavTab from './screens/NavTab';
 import Setting from './screens/ProfileScreen';
 import Book from './screens/BookScreen';
+import DummyPage from './screens/DummyPage'; // Assuming DummyPage is your new component
 
 export default function App() {
   return (
     <div>
-      <BrowserRouter>
+      <Router>
         <NavTab />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Scan" element={<Scan />} />
           <Route path="/Book" element={<Book />} />
           <Route path="/Profile" element={<Setting />} />
+          <Route path="/dummy-page" element={<DummyPage />} /> {/* New route for DummyPage */}
         </Routes>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
