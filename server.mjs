4eb54f4bec70aa12038aa5f9d2817c9b805d172e
Xuggi <godannyyy@gmail.com>
2024-04-12 13:40:13 +0200

    test render

--- server.mjs
+++ server.mjs
@@ -1,9 +1,12 @@
-import 'dotenv/config' ;
-import express from 'express'; 
-import USER_API from './Backend/routes/userRoutes.mjs'; 
-import BOOK_API from './Backend/routes/bookRoutes.mjs'
+import 'dotenv/config';
+import express from 'express';
+import path from 'path'; // Import path module
+import USER_API from './Backend/routes/userRoutes.mjs';
+import BOOK_API from './Backend/routes/bookRoutes.mjs';
 import errorHandler from './Backend/modules/errorHandler.mjs';
-import cors from 'cors'
+import cors from 'cors';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname); // Define __dirname for ES module scope
 
 const server = express();
 server.use(cors());
@@ -11,19 +14,17 @@ server.use(cors());
 const port = (process.env.PORT || 8080);
 server.set('port', port);
 
-
-server.use(express.static('public'));
+// Serve static files from 'src/screens' directory and 'public' directory
+server.use(express.static(path.join(__dirname, 'src', 'screens')));
+server.use(express.static(path.join(__dirname, 'public')));
 
 server.use("/user", USER_API); 
-
 server.use("/book", BOOK_API );
 
-
-
+// Middleware to handle root path ("/")
 server.get("/", (req, res, next) => {
-   req.originalUrl
+   console.log('Requested URL:', req.originalUrl);
    res.status(200).send(JSON.stringify({ msg: "Hello there" })).end();
-
 });
 
 server.use(errorHandler);
@@ -31,5 +32,3 @@ server.use(errorHandler);
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
 });
-
-
