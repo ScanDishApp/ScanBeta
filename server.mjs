e82a105c991e4452015d4f3c924435a7b847f162
Stine Kvist <stinekvist@Stines-Air-2.uia.no>
2024-04-12 13:31:22 +0200

    testing render

--- server.mjs
+++ server.mjs
@@ -14,7 +14,7 @@ const port = process.env.PORT || 8080;
 server.set('port', port);
 
 // Serve static files from the 'src/screens' directory
-server.use(express.static(path.join(__dirname, 'src', 'screens')));
+server.use(express.static('src'));
 
 server.use("/user", USER_API);
 server.use("/book", BOOK_API);
