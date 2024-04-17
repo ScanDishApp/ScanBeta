faf2e468eb8f0e6aa33f2f9522ebaf8db249b35e
Stine Kvist <stinekvist@Stines-Air-2.uia.no>
2024-04-17 14:39:55 +0200

    update

--- src/screens/NewPageScreen.js
+++ src/screens/NewPageScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { AiOutlineFontSize, AiOutlineFontColors, AiOutlineScan, AiOutlinePicture, AiOutlineFileText, AiOutlineArrowLeft, AiOutlineArrowRight, AiOutlineBold, AiOutlineFileAdd, AiOutlineSmile, AiOutlineDelete } from 'react-icons/ai';
+import { AiOutlineFontSize, AiOutlineFontColors, AiOutlineScan, AiOutlinePicture, AiOutlineFileText, AiOutlineArrowLeft, AiOutlineArrowRight, AiOutlineBold, AiOutlineFileAdd, AiOutlineSmile, AiOutlineDelete, AiOutlineInfoCircle } from 'react-icons/ai';
 
 import './ScreenStyle/Home.css';
 import './ScreenStyle/NewPage.css';
@@ -58,11 +58,25 @@ export default function NewPage() {
         setDragging(false);
     };
 
+    const handleRemoveImage = () => {
+        setImage(null); // Set image state to null to remove the image
+        setPosition({ x: 0, y: 0 }); // Reset position
+    };
+
+    const handleDrop = (event) => {
+        event.preventDefault();
+        if (image && position.x !== 0 && position.y !== 0) {
+            // Reset image and position
+            setImage(null);
+            setPosition({ x: 0, y: 0 });
+        }
+    };
+
     useEffect(() => {
-        document.addEventListener('mousemove', handleMouseMove);
-        document.addEventListener('mouseup', handleMouseUp);
-        document.addEventListener('touchmove', handleMouseMove);
-        document.addEventListener('touchend', handleMouseUp);
+        document.addEventListener('mousemove', handleMouseMove, { passive: false });
+        document.addEventListener('mouseup', handleMouseUp, { passive: false });
+        document.addEventListener('touchmove', handleMouseMove, { passive: false });
+        document.addEventListener('touchend', handleMouseUp, { passive: false });
         return () => {
             document.removeEventListener('mousemove', handleMouseMove);
             document.removeEventListener('mouseup', handleMouseUp);
@@ -70,6 +84,7 @@ export default function NewPage() {
             document.removeEventListener('touchend', handleMouseUp);
         };
     }, [dragging]);
+    
 
     useEffect(() => {
         localStorage.setItem('noteTitle', title);
@@ -79,23 +94,27 @@ export default function NewPage() {
     return (
         <div className="NewPage-container">
             <h1>Design din bok</h1>
-    
+
             <div className="navigate-button-container">
                 <button className="back-button"><AiOutlineArrowLeft /></button>
                 <button className="next-button"><AiOutlineArrowRight /></button>
                 <button className="save-button">Lagre</button>
                 <button className="add-page-button"><AiOutlineFileAdd /></button>
+                <button onClick={handleDrop} className="remove-button"><AiOutlineDelete /></button>
+                <button className="info-button"><AiOutlineInfoCircle /></button>
             </div>
-    
+
             <div className="coverPage"></div>
-    
-            
+
             <div className="input-container">
                 {image && (
-                    <div className="image-preview"
+                    <div
+                        className="image-preview"
                         style={{ left: position.x, top: position.y }}
                         onMouseDown={handleMouseDown}
                         onTouchStart={handleMouseDown}
+                        onDrop={handleDrop}
+                        onDragOver={(e) => e.preventDefault()}
                     >
                         <img src={image} alt="Uploaded" />
                     </div>
@@ -121,8 +140,6 @@ export default function NewPage() {
                     onChange={handleContentChange}
                     placeholder="Write your note here..."
                 />
-               
-    
             </div>
             <div className="design-button-container">
                 <button className="scan-button"><AiOutlineScan /></button>
@@ -132,10 +149,7 @@ export default function NewPage() {
                 <button className="font-color-button"><AiOutlineFontColors /></button>
                 <button className="picture-button" onClick={() => document.getElementById('file-input').click()}><AiOutlinePicture /></button>
                 <button className="symbol-button"><AiOutlineSmile /></button>
-                <button className="remove-button"><AiOutlineDelete /></button>
             </div>
-            
         </div >
     );
-    
-}    
+}
